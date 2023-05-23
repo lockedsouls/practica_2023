86d21a6b8fa5b9f038ac7dda9a2935185b4e3997
lockedsouls <lockedssouls@gmail.com>
2023-05-23 09:20:07 +0300

    Add form Title

--- 01_Fitness Club/Fitness Club/Fitness Club/Form1.cs
+++ 01_Fitness Club/Fitness Club/Fitness Club/Form1.cs
@@ -4,15 +4,34 @@ namespace Fitness_Club
 {
     public partial class Form1 : Form
     {
-        SqlConnection db = new SqlConnection(@"Data Source=DESKTOP-B38G05Q;Initial Catalog=fitness_club;Integrated Security=True; TrustServerCertificate=True");
+        public static SqlConnection db = new SqlConnection(@"Data Source=DESKTOP-B38G05Q;Initial Catalog=fitness_club;Integrated Security=True; TrustServerCertificate=True");
+
+        Label title = new Label();
+        Label title2 = new Label();
+        Button login = new Button();
+        Button regis = new Button();
+        Button info = new Button();
         public Form1()
         {
             InitializeComponent();
 
-            SqlDataReader input = new SqlCommand("select * from Clients", db).ExecuteReader();
-            while (input.Read())
+            db.Open();
+            if (db.State == System.Data.ConnectionState.Open)
             {
-                this.Text += $"{input["username"]}  |  ";
+                this.Size = new Size(1000, 700);
+
+                title.Text = "Welcome to ";
+                title.Font = new Font("Segoe UI", 20);
+                title.AutoSize = true;
+                title.Location = new Point(450 - title.Width, 100 - title.Height);
+
+                title2.Text = "Fitness Club!";
+                title2.Font = new Font("Segoe UI", 20, FontStyle.Bold);
+                title2.ForeColor = Color.Blue;
+                title2.AutoSize = true;
+                title2.Location = new Point(title.Location.X + title.Width + 57, title.Location.Y);
+
+                this.Controls.AddRange(new[] {title, title2 });
             }
         }
     }
